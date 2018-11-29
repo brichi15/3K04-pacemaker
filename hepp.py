a3d37b731fc29348d497e2e227af8e84bca4bf3e
kathanpatel1998 <kathan_patel@hotmail.ca>
2018-11-29 15:29:04 -0500

    HEPP UPDATE

--- hepp.py
+++ hepp.py
@@ -1,5 +1,6 @@
 import serial
 import struct
+import time
 
 Par_File = open("Parameters.txt","r")
 Parameters = Par_File.readlines()
@@ -11,76 +12,131 @@ def Load_Values(Username):
     a = []
     b = []
     byte_b = []
-    byte_length = [4,1,1,1,2,1,1,2,2,2,2,2,2,2,2,2,2,1,1,2,1,1,1,1,1,1]
+    byte_length = [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
     
 
     for i in range(0,len(Parameters)):
         a.append(p[i][p[i].find(": ")+2:-1])
         print(p[i][p[i].find(": ")+2:-1])
+        
+    
 
-    for i in a:
-        if i.find('.')>0:
-            b.append(int(round(float(i),1)*1000))
-        elif i == 'OFF':
+    for i in range(0,len(a)):
+        if a[i].find('.')>0:
+            b.append(int(round(float(a[i]),1)*1000))
+        elif a[i] == 'OFF' and i > 0 :
             b.append(0)
-        elif i == 'ON': 
+        elif a[i] == 'ON': 
             b.append(1)
-        elif i.find('High')>=0 or i.find('Low')>=0 or i.find('Med')>=0:
-            if i == "V-Low": b.append(1)
-            if i == "V-Low": b.append(2)
-            if i == "Low": b.append(3)
-            if i == "Med-Low": b.append(4)
-            if i == "Med": b.append(5)
-            if i == "Med-High": b.append(6)
-            if i == "High": b.append(7)
-            if i == "V-High": b.append(8)
-        elif i == "OFF": b.append(0) 
-        elif i == "AAT": b.append(1) 
-        elif i == "VVT": b.append(2)
-        elif i == "AOO": b.append(3)
-        elif i == "AAI": b.append(4)
-        elif i == "VOO": b.append(5)
-        elif i == "VVI": b.append(6)
-        elif i == "VDD": b.append(7)
-        elif i == "DOO": b.append(8)
-        elif i == "DDI": b.append(9)
-        elif i == "DDD": b.append(10)
-        elif i == "AOOR": b.append(11)
-        elif i == "AAIR": b.append(12)
-        elif i == "VOOR": b.append(13)
-        elif i == "VVIR": b.append(14)
-        elif i == "VDDR": b.append(15)
-        elif i == "DOOR": b.append(16)
-        elif i == "DDIR": b.append(17)
-        elif i == "DDDR": b.append(18)
+        elif a[i].find('High')>=0 or a[i].find('Low')>=0 or a[i].find('Med')>=0:
+            if a[i] == "V-Low": b.append(1)
+            if a[i] == "V-Low": b.append(2)
+            if a[i] == "Low": b.append(3)
+            if a[i] == "Med-Low": b.append(4)
+            if a[i] == "Med": b.append(5)
+            if a[i] == "Med-High": b.append(6)
+            if a[i] == "High": b.append(7)
+            if a[i] == "V-High": b.append(8)
+        elif a[i] == "OFF" and i == 0: b.append(bytes(4)) 
+        elif a[i] == "AAT": b.append(a[i]+" ") 
+        elif a[i] == "VVT": b.append(a[i]+" ")
+        elif a[i] == "AOO": b.append(a[i]+" ")
+        elif a[i] == "AAI": b.append(a[i]+" ")
+        elif a[i] == "VOO": b.append(a[i]+" ")
+        elif a[i] == "VVI": b.append(a[i]+" ")
+        elif a[i] == "VDD": b.append(a[i]+" ")
+        elif a[i] == "DOO": b.append(a[i]+" ")
+        elif a[i] == "DDI": b.append(a[i]+" ")
+        elif a[i] == "DDD": b.append(a[i]+" ")
+        elif a[i] == "AOOR": b.append(a[i])
+        elif a[i] == "AAIR": b.append(a[i])
+        elif a[i] == "VOOR": b.append(a[i])
+        elif a[i] == "VVIR": b.append(a[i])
+        elif a[i] == "VDDR": b.append(a[i])
+        elif a[i] == "DOOR": b.append(a[i])
+        elif a[i] == "DDIR": b.append(a[i])
+        elif a[i] == "DDDR": b.append(a[i])
         else:
             try:
                 b.append(int(i))
             except ValueError:
                 b.append(i)
-
-    for i in range(0,26):
+                  
+    byte_b.append(b[0].encode())
+    
+    for i in range(1,26):
         if b[i]>=0:
-            byte_b.append(b[i].to_bytes(byte_length[i], byteorder='big'))
+            byte_b.append(b[i].to_bytes(byte_length[i], byteorder='big',signed=True))
         elif b[i]<0:
             byte_b.append(b[i].to_bytes(byte_length[i], byteorder='big',signed=True))
 
 
 
     print(byte_b)
-    print(b)
-    print(len(a))
+    print(len(byte_b))
+    #print(b)
+    #print(len(a))
 
+
+
+
+    #ser = serial.Serial('COM6', baudrate=115200)
+    #print(ser.name)
+    #cmd = b'\x16\x45\x55\x00'
+    sum1 = 0 
+
+    #WRITE_TEST
+##    '''
+##    cmd = b'\x16\x45\x55\x00'
+##    a = 10
+##    print(ser.write(cmd))
+##    print(ser.write(bytes(50)))
+##    #print(ser.read())
+##    print(cmd)
+##    #print(ser.read(10))
+##    ser.close()
+##    '''
+    #READ_ECHO
+##    cmd = b'\x16\x45\x49'
+##    print(ser.write(cmd))
+##    print(ser.write(bytes(51)))
+##    a = ser.read(50)
+##    print(a)
+##    #print(cmd)
+##    #print(ser.read(10))
+##    ser.close()
+##'''
+##    #READ_EGRRAM
+##    cmd = b'\x16\x45\x47'
+##    print(ser.write(cmd))
+##    print(ser.write(bytes(51)))
+##    print(ser.read(50))
+##    #print(cmd)
+##    #print(ser.read(10))
+##    ser.close()
+##'''    
+    #WRITE_VALUES
+'''    
+    cmd = b'\x16\x45\x55\x04'
+    sum1 = sum1+ ser.write(cmd)
+    for i in range(1,len(byte_b)):
+        print(repr(byte_b[i]))
+        sum1 =sum1 + ser.write(byte_b[i])
+    print("total send:" , sum1)
+
+    cmd1 = b'\x16\x45\x49'
+    print(ser.write(cmd1))
+    print(ser.write(bytes(51)))
+    
+
+    a = ser.read(50)
+    print(a)
+    #print(cmd)
+    #print(ser.read(10))
+    ser.close()
+'''    
+    
 
 Load_Values("oscar")
-
-##ser = serial.Serial('COM6', baudrate=115200)
-##print(ser.name)
-##cmd = b'\x16\x55'
-##
-##ser.write(cmd)
-##for i in byte_b:
-##    ser.write(i)
-##
 ##ser.write(bytes(1))
 ##ser.close()
